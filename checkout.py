09140d0bac5aa3787fd0f65ccc31d5b8afca8163
quesitopre <citlallimartinez122@gmail.com>
2025-10-06 16:22:50 -0700

    installed requests to use APIs

---
+++ checkout.py
@@ -0,0 +1,8 @@
+#from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QMessageBox
+#from PyQt5.QtGui import QFont
+import requests
+
+# class for checkout Process 
+#class CheckoutClick(self):
+print("Checkout Clicked")
+    
