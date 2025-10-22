8bcc2c5bdbac4dddeb7364f6f80854ee7909ff0d
quesitopre <citlallimartinez122@gmail.com>
2025-10-22 15:45:30 -0700

    fixed issues

--- backend/app.py
+++ backend/app.py
@@ -21,15 +21,15 @@ def get_rooms(): #create a route to get all rooms
 def create_room(): 
     data = request.get_json()
 
-new_room = Room(
+    new_room = Room(
         room_number = data['room_number'],
         room_type = data['room_type'],
         price_per_night = data['price_per_night'],
-)
-db.session.add(new_room)
-db.session.commit() #commit the new room to the database
+    ) 
+    db.session.add(new_room)
+    db.session.commit() #commit the new room to the database
 
-return jsonify(new_room.to_dict()),201 #if everything works return the new room with a 201 status code
+    return jsonify(new_room.to_dict()),201 #if everything works return the new room with a 201 status code
 
 if __name__ == "__main__": #add new room to session
     with app.app_context():
