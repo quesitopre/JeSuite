4fa25cf8834f562058586a7f3d91182fdbdfed56
AngelaFernandez12 <angcrisfer@gmail.com>
2025-10-22 15:01:32 -0700

    created method for get to link to db

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, jsonify, request
+from models import db, Room # asking the models to have access to db and Room class
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rooms.db' #database location
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route("/")
+def home():
+    return "Welcome to the Room Management System"
+
+@app.route("/rooms", methods = ['GET'])
+def get_rooms(): #create a route to get all rooms
+    rooms = Room.query.all() #fetch all room records
+    return jsonify([room.to.dict() for room in rooms]) # convert each room to dictionary and return as JSON
+
+
 
