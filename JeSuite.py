84d01241e4a043cfd62be16ae87ad528270ec5b9
msoemoe18 <msoemoe0511@gmail.com>
2025-10-22 20:15:49 -0700

    added the people dropdown menu

--- JeSuite.py
+++ JeSuite.py
@@ -1,7 +1,10 @@
 import sys
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QHBoxLayout, QComboBox, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QPushButton, QWidget, QHBoxLayout, QComboBox, 
+    QVBoxLayout, QLabel, QCalendarWidget
+) 
 from PyQt5.QtGui import QPixmap
 
 
@@ -62,9 +65,15 @@ class MainWindow(QWidget):
 
         #individual layouts for each filter
         location_layout = QVBoxLayout()
-        guest_layout = QVBoxLayout()
-        room_layout = QVBoxLayout()
+        #guest_layout = QVBoxLayout()
+        #room_layout = QVBoxLayout()
         #guestandroom_layout = QVBoxLayout()
+        people_layout = QVBoxLayout()
+        #checkin_layout = QVBoxLayout()
+        #checkout_layout = QVBoxLayout()
+        ##calendar_layout = QVBoxLayout()
+
+        
 
         #location
         location_label = QLabel("Location:")
@@ -72,19 +81,40 @@ class MainWindow(QWidget):
         location_combo.addItems(["Los Angeles, California", "Santa Monica, California", "Beverly Hill, California"])
 
         #guest
-        guest_label = QLabel("Guest:")
-        guest_combo = QComboBox()
-        guest_combo.addItems(["1", "2", "3", "4", "5"])
+        #guest_label = QLabel("Guest:")
+        #guest_combo = QComboBox()
+        #guest_combo.addItems(["1", "2", "3", "4", "5"])
 
         #room
-        room_label = QLabel("Room:")
-        room_combo = QComboBox()
-        room_combo.addItems(["1", "2", "3", "4", "5"])
+        #room_label = QLabel("Room:")
+        #room_combo = QComboBox()
+        #room_combo.addItems(["1", "2", "3", "4", "5"])
 
         #guestandroom
         #guestandroom_label = QLabel("Guest and Room:")
         #guestandroom_combo = QComboBox()
-        #guestandroom_combo.addItems(["1 guest, 1 room", "2 guests, 1 room", "3 guests, 2 rooms", "4 guests, 2 rooms", "5 guests, family room"])
+        #guestandroom_combo.addItems(["1 guest, 1 room", "2 guests, 1 room", "3 guests, 1 room", "4 guests, 1 room", "5 guests, 2 rooms"])
+
+        #calendar checkin
+        ##checkin_label = QLabel("Check In Date:")
+        ##self.checkin_calendar = QCalendarWidget(self)
+        ##self.checkin_calendar.setGridVisible(True)
+        #self.checkin_calendar.setCalendar(QDate.currentDate())
+        #self.checkin_calendar.setCalendarPopup(True)
+
+
+        #calendar checkout
+         #checkout_label = QLabel("Check Out Date:")     
+         #self.checkout_calendar = QCalendarWidget(self)
+         #self.checkout_calendar.setGridVisible(True)
+        #self.checkout_calendar.setCalendar(QDate.currentDate().addDay(1))
+        #self.checkout_calendar.setCalendarPopup(True)
+
+
+        #people
+        people_label = QLabel("People:")
+        people_combo = QComboBox()
+        people_combo.addItems(["1 guest, 1 room", "2 guests, 1 room", "3 guests, 1 room", "4 guests, 1 room", "5 guests, 2 rooms", "6 guests, 2 rooms"])
 
 
         #searchbutton
@@ -96,25 +126,35 @@ class MainWindow(QWidget):
         filter_layout.addLayout(location_layout)
 
         #adding guest layout to filter layout
-        guest_layout.addWidget(guest_label)
-        guest_layout.addWidget(guest_combo)
-        filter_layout.addLayout(guest_layout)
+        #guest_layout.addWidget(guest_label)
+        #guest_layout.addWidget(guest_combo)
+        #filter_layout.addLayout(guest_layout)
 
         #adding room layout to filter layout
-        room_layout.addWidget(room_label)
-        room_layout.addWidget(room_combo)
-        filter_layout.addLayout(room_layout)
+        #room_layout.addWidget(room_label)
+        #room_layout.addWidget(room_combo)
+        #filter_layout.addLayout(room_layout)
 
         #adding guestandroom layout to filter layout
         #guestandroom_layout.addWidget(guestandroom_label)
         #guestandroom_layout.addWidget(guestandroom_combo)
         #filter_layout.addLayout(guestandroom_layout)
 
+        #adding people layout to filter layout
+        people_layout.addWidget(people_label)
+        people_layout.addWidget(people_combo)
+        filter_layout.addLayout(people_layout)
 
-        #adding calendar layout to filter layout
-        #calendar_layout.addWidget(checkin_label)
-        #calendar_layout.addWidget(_combo)
-        #filter_layout.addLayout(_layout)
+        #adding calendar check_in layout to filter layout
+        ##calendar_layout.addWidget(checkin_label)
+        ##calendar_layout.addWidget(self.checkin_calendar)
+        ##filter_layout.addLayout(calendar_layout)
+
+        #adding calendar check_out layout to filter layout
+        #calendar_layout.addWidget(checkout_label)
+        #calendar_layout.addWidget(self.checkout_calendar)
+        #filter_layout.addLayout(calendar_layout)
+
 
         filter_layout.addWidget(search_button)
         #filter_layout.addWidget(self.label)
