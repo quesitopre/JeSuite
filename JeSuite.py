132aaa38bc81df15cb2cd68e12ef76047b3ed7e5
marriiaa-j <marriajimenezz@gmail.com>
2025-11-05 20:12:20 -0800

    Update calendar feature

--- JeSuite.py
+++ JeSuite.py
@@ -7,6 +7,33 @@ from PyQt5.QtWidgets import (
 ) 
 from PyQt5.QtGui import QPixmap
 
+#calendar popup class
+class CalendarPopUp(QCalendarWidget):
+    #Popup calendar for selecting start and end dates
+    def __init__(self, parent=None, callback=None):
+        super().__init__(parent)
+        self.callback = callback
+        self.setWindowFlags(Qt.Popup)
+        self.clicked.connect(self.on_date_selected)#typo fixed
+        self.start_date = None #The first click
+
+    def on_date_selected(self, date):
+        #Select the starting and ending dates, then close ze popup
+        if not self.start_date:
+            self.start_date = date
+        else:
+            end_date = date
+            #verify date order
+            if end_date <self.start_date:
+                self.start_date, end_date = end_date, self.start_date
+            if self.callback:
+                self.callback(self.start_date,end_date)
+                self.close( )
+
+
+        
+
+
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QWidget): 
@@ -33,6 +60,8 @@ class MainWindow(QWidget):
         main_layout.addWidget(self.label)
         main_layout.addLayout(filters)
         self.setLayout(main_layout)
+
+        self.calendar_popUp = None
 
     def create_header(self):
         header_layout = QHBoxLayout()
@@ -71,7 +100,7 @@ class MainWindow(QWidget):
         people_layout = QVBoxLayout()
         #checkin_layout = QVBoxLayout()
         #checkout_layout = QVBoxLayout()
-        ##calendar_layout = QVBoxLayout()
+        calendar_layout = QVBoxLayout()
 
         
 
@@ -95,28 +124,21 @@ class MainWindow(QWidget):
         #guestandroom_combo = QComboBox()
         #guestandroom_combo.addItems(["1 guest, 1 room", "2 guests, 1 room", "3 guests, 1 room", "4 guests, 1 room", "5 guests, 2 rooms"])
 
-        #calendar checkin
-        ##checkin_label = QLabel("Check In Date:")
-        ##self.checkin_calendar = QCalendarWidget(self)
-        ##self.checkin_calendar.setGridVisible(True)
-        #self.checkin_calendar.setCalendar(QDate.currentDate())
-        #self.checkin_calendar.setCalendarPopup(True)
 
-
-        #calendar checkout
-         #checkout_label = QLabel("Check Out Date:")     
-         #self.checkout_calendar = QCalendarWidget(self)
-         #self.checkout_calendar.setGridVisible(True)
-        #self.checkout_calendar.setCalendar(QDate.currentDate().addDay(1))
-        #self.checkout_calendar.setCalendarPopup(True)
-
+        #calendar
+        calendar_label = QLabel("Calendar:")
+        calendar_combo = QComboBox()
+        calendar_combo.addItems(["Check-in and Check-out Dates"])
+        
 
         #people
         people_label = QLabel("People:")
         people_combo = QComboBox()
         people_combo.addItems(["1 guest, 1 room", "2 guests, 1 room", "3 guests, 1 room", "4 guests, 1 room", "5 guests, 2 rooms", "6 guests, 2 rooms"])
 
-
+        #datebutton
+        self.date_button = QPushButton("Select Dates")
+        self.date_button.clicked.connect(self.show_calendar_popup)
         #searchbutton
         search_button = QPushButton("Search")
 
@@ -146,9 +168,9 @@ class MainWindow(QWidget):
         filter_layout.addLayout(people_layout)
 
         #adding calendar check_in layout to filter layout
-        ##calendar_layout.addWidget(checkin_label)
-        ##calendar_layout.addWidget(self.checkin_calendar)
-        ##filter_layout.addLayout(calendar_layout)
+        calendar_layout.addWidget(calendar_label)
+        calendar_layout.addWidget(self.date_button) #bug fixed
+        filter_layout.addLayout(calendar_layout)
 
         #adding calendar check_out layout to filter layout
         #calendar_layout.addWidget(checkout_label)
@@ -156,11 +178,25 @@ class MainWindow(QWidget):
         #filter_layout.addLayout(calendar_layout)
 
 
+
         filter_layout.addWidget(search_button)
         #filter_layout.addWidget(self.label)
         
         return filter_layout
-        
+    
+    def show_calendar_popup(self):
+        if self.calendar_popUp and self.calendar_popUp.isVisible():
+            self.calendar_popUp.close()
+            return
+        self.calendar_popUp= CalendarPopUp(self, self.set_selected_dates)
+        button_pos =self.date_button.mapToGlobal(self.date_button.rect().bottomLeft())
+        self.calendar_popUp.move(button_pos)
+        self.calendar_popUp.show()
+
+    def set_selected_dates(self, start_date, end_date):
+        self.date_button.setText(
+            f"{start_date.toString('MM/dd/yyyy')} - {end_date.toString('MM/dd/yyyy')}"
+            )
 
 app = QApplication(sys.argv)
 
