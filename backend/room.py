cb409c473fd1f7f934d1bfc5ee3f3ebd35e08c08
AngelaFernandez12 <angcrisfer@gmail.com>
2025-11-07 17:49:14 -0800

    AF 11/07/25 5:49

---
+++ backend/room.py
@@ -0,0 +1,36 @@
+# this is the room file
+from dataclasses import dataclass, field
+from typing import List, Optional
+from homepage import CalendarPopUp, MainWindow
+
+class Room:
+    
+    def __init__(self, bed_type, num_beds, price_per_night, room_num, is_available):
+        self.room_number = 0
+        #the room type shows if customer wants a single with either a queen or king bed, a double with two beds, or a suite
+        self.room_type = ["Suite", "Double", "Single_Queen", "Single_King"]
+        self.price_per_night = 0.0
+        self.hotel_amenities = ["Pool", "Gym", "Free Breakfast", "Wifi", "Free Parking", "Office Area", "Laundry"]
+        self.room_amenitites = ["Kitchen", "TV", "Room Service"]
+        self.is_available = True
+        self.check_in_date = None
+        self.check_out_date = None
+
+
+    def set_room(self, bed_type, num_beds, price_per_night, room_num, is_available):
+        self.bed_type = bed_type
+        self.num_beds = num_beds
+        self.price_per_night = price_per_night
+        self.room_num = room_num
+        self.is_available = is_available
+
+    
+
+
+        
+
+
+
+
+       
+    
