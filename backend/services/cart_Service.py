22caec27f54347fef50d89616e800dc0782e81e0
quesitopre <citlallimartinez122@gmail.com>
2025-11-09 16:27:29 -0800

    commented func structure

--- backend/services/cart_Service.py
+++ backend/services/cart_Service.py
@@ -5,3 +5,20 @@ creditCard = 123
 cvv = 
 CustomerEmail = 
 bookingID = 
+
+
+#cart function to remove item from cart
+ #need a pop() function here
+
+#cart function to add item to cart
+#Need an append() function here
+
+#cart function to delete items in the cart
+#clear function here clear()
+
+# cart function to calculate the total price of items in the cart
+
+
+#cart function to view items in the cart
+# need to return number of items in cart and details of items in cart
+#length function here len((listName())
