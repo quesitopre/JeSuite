dc6415b618880ddd3747eb1d497c4b2916c0025c
AngelaFernandez12 <angcrisfer@gmail.com>
2025-11-20 21:21:04 -0800

    pydoc comments added

--- backend/roomSearch.py
+++ backend/roomSearch.py
@@ -4,6 +4,10 @@ from homepage import CalendarPopUp, MainWindow
 from backend.room import Room
 
 class roomSearch:
+
+    '''
+    This __init__ has the objects that we created from the room class
+    '''
     def __init__(self):
         self.single_queen = Room()
         self.single_queen.set_room("Queen", 1, 120.0, 209, True)
@@ -16,7 +20,20 @@ class roomSearch:
 
         self.suite = Room()
         self.suite.set_room("King", 2, 280, 412, True)
-#add in the buttons later for popup
+
+    '''
+    This def, selected_people uses the homepage
+
+    It determines which button was selceted in the front end where the user decides
+    how many people and rooms they want
+
+    It selects by treating it like an array and returning certain rooms depeding on
+    the button(index of the array) selected
+
+    Users who choose 1-4 guests must select one room
+    Users who choose 5-6 guests must select two rooms
+    
+    '''
     def selected_people(self):
         if self.people_combo.currentIndex() == 0:
             return {self.single_queen, self.single_king, self.suite}
