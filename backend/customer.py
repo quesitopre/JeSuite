73cbebc2ddd6515f51a28be6b3511d34ff64c4d0
AngelaFernandez12 <angcrisfer@gmail.com>
2025-11-22 22:42:53 -0800

    vars now have values from user input

--- backend/customer.py
+++ backend/customer.py
@@ -3,25 +3,70 @@ from dataclasses import dataclass, field
 from typing import List, Optional
 from homepage import CalendarPopUp, HomePage
 from backend.room import Room
+from reservationPage import TextInputField, ReservationPage
 import os
 import csv
 
 
 class Customer:
-    '''
-    The __init__ holds all the customer profile variables
-    It currently has place holder strings until the final page is finished and I can swap for
-    the real variables from the user input
-    '''
-    def __init__(self):
-        #make protected
-        #use in email class later
-        self.customer_first_name = "Button Customer first name"
-        self.customer_last_name = "Button customer last name"
-        self.credit_card_num = "Button credit card number"
-        self.credit_card_cvc = "Button credit card cvc"
-        self.billing_zip_code = "Button billing zip code"
-        self.customer_email = "Button customer email"
-        self.check_in_date = "txt from check in button"
-        self.check_out_date = "txt from check out button"
+    """
+    Represents a customer making a reservation.
+    """
 
+    def __init__(self, first_name: str, last_name: str, phone: str, email: str,
+                 address: str, city: str, state: str, zipcode: str,
+                 card_num: str, expir_month: str, expir_year: str,
+                 cvv: str, billing_zip: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.card_num = card_num
+        self.expir_month = expir_month
+        self.expir_year = expir_year
+        self.cvv = cvv
+        self.billing_zip = billing_zip
+
+    def get_contact_info(self) -> dict:
+        #Return safe customer contact info
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone": self.phone,
+            "email": self.email,
+            "address": f"{self.address}, {self.city}, {self.state} {self.zipcode}"
+        }
+
+    def get_payment_info(self) -> dict:
+        #Return payment info
+        return {
+            "card_num": self.card_num,
+            "expir_month": self.expir_month,
+            "expir_year": self.expir_year,
+            "cvv": self.cvv,
+            "billing_zip": self.billing_zip
+        }
+def confirm_reservation(self):
+    #Handle reservation confirmation logic
+
+    customer = Customer(
+        first_name=self.first_name_field.get_value(),
+        last_name=self.last_name_field.get_value(),
+        phone=self.phone_field.get_value(),
+        email=self.email_field.get_value(),
+        address=self.addressline_field.get_value(),
+        city=self.city_field.get_value(),
+        state=self.state.get_value(),
+        zipcode=self.zipcode.get_value(),
+        card_num=self.card_num.get_value(),
+        expir_month=self.expir_month.get_value(),
+        expir_year=self.expir_year.get_value(),
+        cvv=self.CVV.get_value(),
+        billing_zip=self.billing_zip.get_value()
+    )
+
+    
