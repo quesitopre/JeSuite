6b69ff9c179244a6a02a8277778151e49e91f7cb
AngelaFernandez12 <angcrisfer@gmail.com>
2025-12-03 11:39:46 -0800

    k

--- backend/services/test_CartService.py
+++ backend/services/test_CartService.py
@@ -2,4 +2,8 @@ import unittest #The test framework
 from cart_Service import CartService, CartItem
 from room import Room
 from datetime import datetime
+from roomSearch import roomSearch
 
+class TestCartService:
+    print("hello world")
+   # print(self.cartItems) 
